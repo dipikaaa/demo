773cda9fe59cde9932aa682708f6f78e1d9e5af4
dipika <dipikaj@cybage.com>
2015-10-20 14:43:10 +0530

    added two more files

---
+++ src/typeof.js
@@ -0,0 +1,11 @@
+//demo code for typeof operator
+var a=10;
+var b="dipika";
+
+var result = ( typeof a == "string" ? "A is string" : " A is numeric");
+document.write("Result =>");
+document.write(result);
+document.write("<br>");
+var result = ( typeof b == "string" ? "B is string" : " B is numeric");
+document.write("Result =>");
+document.write(result);
